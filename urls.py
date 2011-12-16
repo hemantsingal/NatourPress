82ca2561b7444dcede27b448b7cbfe5f76534225
Hemant Singal <hemants.iitk@gmail.com>
2011-12-16 13:58:11 +0530

    main files

--- urls.py
+++ urls.py
@@ -1,8 +1,8 @@
 from django.conf.urls.defaults import patterns, include, url
 
 # Uncomment the next two lines to enable the admin:
-# from django.contrib import admin
-# admin.autodiscover()
+from django.contrib import admin
+admin.autodiscover()
 
 urlpatterns = patterns('',
     # Examples:
@@ -13,5 +13,9 @@ urlpatterns = patterns('',
     # url(r'^admin/doc/', include('django.contrib.admindocs.urls')),
 
     # Uncomment the next line to enable the admin:
-    # url(r'^admin/', include(admin.site.urls)),
+    url(r'^config/', include('natourpress.urls')),
+    url(r'^admin/', include(admin.site.urls)),
+   # url(r'^login', 'natourpress.views.my_login'),
+    (r'^login', 'django.contrib.auth.views.login',{'template_name': 'natourpress/login.html'}),
+    (r'^logout', 'natourpress.views.logout_view'),
 )
